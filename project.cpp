a9bcbc9cae257f69bd0e1e1069c0d1e7179c8447
Sambari Manikanta <sambarapumani@gmail.com>
2021-01-18 09:32:55 +0530

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,17 +1,22 @@
-#include<iostream>
-#include<sstream>
+#include <iostream> 
+#include <sstream> 
 #include<typeinfo>
-using namespace std;
-
-int main(){
-	string input;
-	cout<<"Enter the number";
-	cin>>input;
-	cout << typeid(input).name();
-//	cout<< typeof(input);
-	int number[100];
-	number = atoi(input);
-	//cout<< typeof(number);
-	cout << typeid(number).name() << endl;
-	cout<<input<<":"<<number;
+using namespace std; 
+  
+int main() 
+{ 
+    string input;
+    cout<<"Enter the Number: ";
+    cin>>input;
+  	cout << "\nType of Input: " <<typeid(input).name(); //prints the type of Input 
+    // object from the class stringstream 
+    stringstream ss(input); 
+  
+    int number = 0; 
+    ss >> number; 
+    cout <<"\nType of number: " <<typeid(number).name() << endl; //prints the type of number 
+    // Now the variable x holds the value 12345 
+    cout << "Value of number : " << number; 
+  
+    return 0; 
 }
