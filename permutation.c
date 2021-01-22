4bd9130809d1e7fa1889e42a6614f1a3da6e7ce8
monsTa-b0y <77019660+monsTa-b0y@users.noreply.github.com>
2021-01-22 22:20:47 +0530

    Update permutation.c

--- permutation.c
+++ permutation.c
@@ -30,9 +30,23 @@ void permute(char *a, int l, int r) {
  
 /* Driver program to test above functions */
 int main() { 
-    char str[26];
-    printf("Enter the String:");
-    gets(str);
+    char str[26] = "";
+    int option;
+        printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+        printf("Enter your option:");
+        scanf("%d",&option);
+        switch (option){
+            case 1 : strcpy(str,"");break;
+            case 2 : strcpy(str,"ABC");break;
+            case 3 : strcpy(str,"DEF");break;
+            case 4 : strcpy(str,"GHI");break;
+            case 5 : strcpy(str,"JKL");break;
+            case 6 : strcpy(str,"MNO");break;
+            case 7 : strcpy(str,"PQRS");break;
+            case 8 : strcpy(str,"TUV");break;
+            case 9 : strcpy(str,"WXYZ");break;
+            default : printf("Invalid Selection\n");
+        }
     int n = strlen(str); 
     permute(str, 0, n-1); 
     return 0; 
